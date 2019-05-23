55155815ae00c57f024244996d1b48a29613379e
Noah Flint <noah.flint24@gmail.com>
2019-05-23 12:43:24 -0600

    capitialized address class in model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :organizer, foreign_key: 'organizer_id', class_name: :User
-  has_one :location, as: :owner, class_name: :address
+  has_one :location, as: :owner, class_name: :Address
   has_many :carpools
 
-  def address
-    Address.where(owner_id: id).first
-  end
+  # def address
+  #   Address.where(owner_id: id).first
+  # end
 end
