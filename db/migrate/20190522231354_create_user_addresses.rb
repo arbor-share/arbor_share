affc72b2dd14fe7285213a47588d05dd605dd59a
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-22 18:16:15 -0600

    Purge UserAddresses table as unnecessary

--- db/migrate/20190522231354_create_user_addresses.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserAddresses < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_addresses do |t|
-      t.references :user, foreign_key: true
-      t.references :address, foreign_key: true
-    end
-  end
-end
