38b32263fe03033707bca89491879f048ff4237a
Noah Flint <noah.flint24@gmail.com>
2019-05-23 16:34:15 -0600

    added test for sorting title a-z

--- spec/features/user/project_index_sorting_spec.rb
+++ spec/features/user/project_index_sorting_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe 'as a logged in user' do
                               role: 1,
                               active: true)
 
-      @project1 = Project.create(title: 'Project 1',
+      @project1 = Project.create(title: 'A Project 1',
                                   date: '2019-07-30', 
                                   description: 'Description of Project 1',
                                   image: 'http://clipart-library.com/image_gallery/104074.png',
                                   organizer: @organizer,
                                   active: true)
 
-      @project2 = Project.create(title: 'Project 2',
+      @project2 = Project.create(title: 'C Project 2',
                                   date: '2019-06-30', 
                                   description: 'Description of Project 2',
                                   image: 'http://clipart-library.com/image_gallery/104074.png',
                                   organizer: @organizer,
                                   active: true)
 
-      @project3 = Project.create(title: 'Project 3',
+      @project3 = Project.create(title: 'B Project 3',
                             date: '2019-05-30', 
                             description: 'Description of Project 3',
                             image: 'http://clipart-library.com/image_gallery/104074.png',
@@ -64,6 +64,13 @@ RSpec.describe 'as a logged in user' do
       within '.projects' do
         expect(@project3.title).to appear_before(@project1.title)
         expect(@project2.title).to appear_before(@project1.title)
+      end
+    end
+
+    it 'can sort the projects by title a-z ' do
+      within '.projects' do
+        expect(@project1.title).to appear_before(@project3.title)
+        expect(@project3.title).to appear_before(@project2.title)
       end
     end
   end
