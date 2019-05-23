3f20735cada54ffa2a2115da8a5548c8386cb09b
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-22 18:23:06 -0600

    Ensure user emails unique at DB level

--- db/migrate/20190522230803_create_users.rb
+++ db/migrate/20190522230803_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :full_name
-      t.string :email
+      t.string :email, unique: true
       t.string :about
       t.string :avatar_image
       t.string :google_token
