1ced9cb2d59759f7639eacc1a865434fdb0af07f
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-22 18:16:42 -0600

    Rename Blocklist table to UserBlocks

--- db/migrate/20190522234451_create_user_blocks.rb
+++ db/migrate/20190522234451_create_user_blocks.rb
@@ -1,6 +1,6 @@
-class CreateBlocklist < ActiveRecord::Migration[5.2]
+class CreateUserBlocks < ActiveRecord::Migration[5.2]
   def change
-    create_table :blocklist do |t|
+    create_table :user_blocks do |t|
       t.references :blocking_user, foreign_key: { to_table: :users }
       t.references :blocked_user, foreign_key: { to_table: :users }
     end
