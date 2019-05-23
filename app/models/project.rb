ee748b5e132307edcba1e83e56f789dae5b49130
Noah Flint <noah.flint24@gmail.com>
2019-05-23 08:45:34 -0600

    added address method for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :organizer, foreign_key: 'organizer_id', class_name: :User
   has_one :location, as: :owner, class_name: :address
   has_many :carpools
+
+  def address
+    Address.where(owner_id: id).first
+  end
 end
