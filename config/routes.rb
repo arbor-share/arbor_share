2826b02da8b4398c7b0d870374d6849e59121223
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-23 13:48:56 -0600

    Add user#new view and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/policy', to: 'policy#index'
   get '/auth/:provider/callback', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
 end
