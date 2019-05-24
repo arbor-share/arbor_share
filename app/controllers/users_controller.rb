37b0beec8254343496c3fa1e13e75440ee3326cf
Noah Flint <noah.flint24@gmail.com>
2019-05-24 17:29:49 -0600

    added profile show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
   def new
-   render locals: {
-     new_user: User.new(session[:oauth_details])
-   }
+    render locals: {
+      new_user: User.new(session[:oauth_details])
+    }
   end
 
+  def show
+    require 'pry'; binding.pry
+  end
+  
   def create
-    new_user = User.new(user_params)
+    new_user = User.new!(user_params)
     if new_user.save
+    require 'pry'; binding.pry
+      
       session[:user_id] = new_user.id
       session[:oauth_details] = nil
     else
