dfe5e29ef0c8dda82ef888bdd1970907bca4b010
Noah Flint <noah.flint24@gmail.com>
2019-05-24 11:22:45 -0600

    added conditional for params[:needds_driver]

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,11 @@ class ProjectsController < ApplicationController
     @projects = Project.where(active: true).order(:date)
     if params[:sort_by] == 'a-z'
       @projects = Project.where(active: true).order(:title)
+    elsif params[:sort_by] == 'needs_driver'
+      needs_driver = @projects.find_all do |project|
+        project.carpools.empty?
+      end
+      @projects = needs_driver 
     else
     end
   end
