e95a2d4a87db97556bd2e4da404e530901ef6ca1
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-24 12:58:45 -0600

    Add Project relationship tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   describe 'Relationships:' do
     it{ should belong_to(:organizer) }
+    it{ should have_one(:location) }
+    it{ should have_many(:carpools) }
   end
 
   describe 'Validations:' do
