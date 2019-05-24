9ce3cf6a84e5bdda50714a5353b18e2ebb45b371
Noah Flint <noah.flint24@gmail.com>
2019-05-24 17:51:38 -0600

    added class method test for sorting

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  describe 'Class Methods' do
+    it '.sorted' do
+      @organizer = User.create(full_name: 'Project Organizer',
+                              email: 'organizer@email.com',
+                              about: 'A little about myself, very little',
+                              avatar_image: 'link to image',
+                              google_token: 'google token',
+                              google_id: 1,
+                              role: 1,
+                              active: true)
+
+      @project1 = Project.create(title: 'A Project 1',
+                                  date: '2019-07-30', 
+                                  description: 'Description of Project 1',
+                                  image: 'http://clipart-library.com/image_gallery/104074.png',
+                                  organizer: @organizer,
+                                  active: true)
+
+      @project2 = Project.create(title: 'C Project 2',
+                                  date: '2019-06-30', 
+                                  description: 'Description of Project 2',
+                                  image: 'http://clipart-library.com/image_gallery/104074.png',
+                                  organizer: @organizer,
+                                  active: true)
+
+      @project3 = Project.create(title: 'B Project 3',
+                            date: '2019-05-30', 
+                            description: 'Description of Project 3',
+                            image: 'http://clipart-library.com/image_gallery/104074.png',
+                            organizer: @organizer,
+                            active: true)
+
+      @address1 = Address.create(owner: @project1,
+                              line_1: "first address line",
+                              line_2: "second address line",
+                              city: "city town",
+                              state: 0,
+                              zip: '12345',
+                              default: true)
+
+      @address2 = Address.create(owner: @project2,
+                              line_1: "first address line",
+                              line_2: "second address line",
+                              city: "city town",
+                              state: 0,
+                              zip: '12345',
+                              default: true)
+
+      @address3 = Address.create(owner: @project3,
+                              line_1: "first address line",
+                              line_2: "second address line",
+                              city: "city town",
+                              state: 0,
+                              zip: '12345',
+                              default: true)
+
+      # alphabetically = Project.sorted("a-z")
+      # Project.sorted("date")
+      # Project.sorted("no_driver")
+      # require 'pry'; binding.pry
+    end
+  end
+end
