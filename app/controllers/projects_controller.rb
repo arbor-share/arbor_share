572793e33ae3a62b3935805143dad568f63d5602
Noah Flint <noah.flint24@gmail.com>
2019-05-23 20:05:11 -0600

    added conditional for sort params a-z

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,10 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.where(active: true).order(:date)
+    if params[:sort_by] == 'a-z'
+      @projects = Project.where(active: true).order(:title)
+    else
+    end
   end
 
   def show
