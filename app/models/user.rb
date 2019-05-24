a6e11dd3a09c6a68f6c789d9554d1e0e4231d055
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-24 13:05:04 -0600

    Add 'rides' relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
 
   has_many :user_blocks
   has_many :blocked_users, through: :user_blocks
+
   has_many :carpools, foreign_key: 'driver_id'
+  has_many :carpool_passengers
+  has_many :rides, through: :carpool_passengers, class_name: :Carpool
 
   enum role: %i[default organizer admin]
 
-  validates :email, 
+  validates :email,
             presence: true,
             format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
