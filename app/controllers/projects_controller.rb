e75f0ba58a386f391823b26d34c58982f1b7a6fb
Noah Flint <noah.flint24@gmail.com>
2019-05-26 12:17:57 -0600

    refactored controller to user .sorted class method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,12 @@
 class ProjectsController < ApplicationController
 
   def index
-
-    @projects = Project.sorted(params[:sort_by])
-    
-    @projects = Project.where(active: true).order(:date)
     if params[:sort_by] == 'a-z'
-      @projects = Project.where(active: true).order(:title)
+      @projects = Project.sorted(params[:sort_by])
     elsif params[:sort_by] == 'needs_driver'
-      needs_driver = @projects.find_all do |project|
-        project.carpools.empty?
-      end
-      @projects = needs_driver 
+      @projects = Project.sorted(params[:sort_by])
     else
+      @projects = Project.sorted
     end
   end
 
