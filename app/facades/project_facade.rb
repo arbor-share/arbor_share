dc289953e0101e336f9c8ee7c7ee10b95eb92ce7
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-25 18:54:43 -0600

    Adds project facade to format info

---
+++ app/facades/project_facade.rb
@@ -0,0 +1,11 @@
+class ProjectFacade
+  attr_reader :title, :date, :image, :location, :carpools, :description
+  def initialize(project)
+    @title = project.title
+    @description = project.description
+    @date = project.date
+    @image = project.image
+    @location = project.location
+    @carpools = project.carpools
+  end
+end
