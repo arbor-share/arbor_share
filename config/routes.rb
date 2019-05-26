0fa8e1b1f50916f8ae019c41d1471db9880b590e
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-26 14:21:33 -0600

    Add route to update vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create] do
     resources :addresses, only: [:update]
+    resources :vehicles, only: [:update]
   end
 
   namespace :profile do
