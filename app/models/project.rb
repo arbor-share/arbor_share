6874feb789968f8473e1ab92126da4d14ad16e32
Noah Flint <noah.flint24@gmail.com>
2019-05-26 11:54:45 -0600

    created .sorted class method fot project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ApplicationRecord
   has_one :location, as: :owner, class_name: :Address
   has_many :carpools
 
+
+  def self.sorted(sort)
+    if sort == "a-z"
+      Project.where(active: true).order(:title)
+    else
+    end
+  end
+
 end
