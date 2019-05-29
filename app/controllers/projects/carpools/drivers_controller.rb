f2caf4a5d0abbe4c14b4062638436d3d12e82ea0
Noah Flint <noah.flint24@gmail.com>
2019-05-28 18:28:11 -0600

    added nested drivers controller

---
+++ app/controllers/projects/carpools/drivers_controller.rb
@@ -0,0 +1,8 @@
+# class Projects::Carpools::DriversController < ApplicationController
+  
+#   def destroy
+#     require 'pry'; binding.pry
+#     @project = Project.find(params[:id])
+#     @carpool = Carpool.find(params[:carpool_id])
+#   end
+# end
