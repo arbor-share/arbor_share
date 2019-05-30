a51a0f93d1e4a684337a91e615738eab32cf8d06
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-30 10:59:32 -0600

    Creates carpool facade and builds geojson api call for map routing in carpool page

--- app/services/mapbox_service.rb
+++ app/services/mapbox_service.rb
@@ -6,6 +6,13 @@ class MapboxService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def get_geo(coord_list)
+    conn = Faraday.new('https://api.mapbox.com/directions/v5/mapbox/driving/')
+    response = conn.get("#{coord_prep(coord_list) + '?access_token=' + api_key + '&geometries=geojson'}")
+    geo_json = JSON.parse(response.body, symbolize_names: true)
+    geo_json[:routes].first[:geometry][:coordinates]
+  end
+
   private
 
   def api_key
@@ -14,5 +21,11 @@ class MapboxService
 
   def address_prep(address)
     (address.line_1 + '%20' + address.city + '%20' + address.state + '%20' + address.zip).gsub(/[' ']/, '%20')
+  end
+
+  def coord_prep(coords)
+    coords.map do |long, lat|
+      "#{long},#{lat};"
+    end.join.sub(/;\z/, '')
   end
 end
