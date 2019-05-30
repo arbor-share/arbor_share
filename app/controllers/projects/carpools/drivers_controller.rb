724a223fdc9695e3f48a4bc6f351954c7f94fa6e
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-29 20:02:08 -0600

    Add link to email notifications. Renamed mailer files to match method.
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- app/controllers/projects/carpools/drivers_controller.rb
+++ app/controllers/projects/carpools/drivers_controller.rb
@@ -11,19 +11,4 @@ class Projects::Carpools::DriversController < ApplicationController
     redirect_to project_path(project)
   end
 
-  def passengers_emails(carpool_id)
-
-    # passengers = User.select(:email)
-    #                  .joins(carpool_passengers: :carpools)
-    #                  .where("carpools.id = ?, #{carpool_id}")
-
-    # SELECT users.email from users
-	  #  JOIN carpool_passengers
-	  #   ON users.id = carpool_passengers.passenger_id
-	  #  JOIN carpools
-	  #   ON carpool_passengers.carpool_id = carpools.id;
-    #  WHERE carpools.id = 1;
-
-  end
-
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,8 @@
 class NotificationMailer < ApplicationMailer
 
   def driver_cancelled_carpool(passengers_emails, project)
-    # @url  = '/'
     @url  = default_url_options[:host]
-    #change to direct to project show page
     @project = project
-    binding.pry
     mail(to: passengers_emails, subject: 'Your Ride Was Cancelled')
   end
 
