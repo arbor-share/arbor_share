5ccfb4f0cb6df9e95b7b9f7222df09582604ba34
Jon Peterson <github@joequincy.dev>
2019-11-09 16:28:57 -0700

    Ruby has instance variable syntax

--- spec/features/admins/projects/edit_spec.rb
+++ spec/features/admins/projects/edit_spec.rb
@@ -3,7 +3,7 @@ require 'date'
 
 describe 'As an admin' do
   before :each do
-    future_date = (Date.today >> 1).strftime('%Y-%m-%d')
+    @future_date = (Date.today >> 1).strftime('%Y-%m-%d')
     @admin = User.new(full_name: "Vincent",
                      email: "vincent@example.com",
                      about: "TBD",
@@ -12,7 +12,7 @@ describe 'As an admin' do
                      google_id: nil,
                      role: 2,
                      active: true)
-     @project_1 = Project.create!(title: 'Shelf Lake Trail Restoration', date: future_date, organizer: @admin, description: "You'll work to improve a half-mile section of the popular Shelf Lake Trail. This project is relatively small with 20-25 volunteers and tasks may be technical in nature as you install water bars, check steps, and other drainage structures as well as build new rock step stream crossings. If you are new to trail work, don't worry! No previous experience is required; just be prepared to work hard at high altitude among classic Colorado views.", active: true, image: 'https://www.voc.org/sites/default/files/styles/760x420/public/op_images/ShelfLake_8.jpg?itok=_ZH6V4li')
+     @project_1 = Project.create!(title: 'Shelf Lake Trail Restoration', date: @future_date, organizer: @admin, description: "You'll work to improve a half-mile section of the popular Shelf Lake Trail. This project is relatively small with 20-25 volunteers and tasks may be technical in nature as you install water bars, check steps, and other drainage structures as well as build new rock step stream crossings. If you are new to trail work, don't worry! No previous experience is required; just be prepared to work hard at high altitude among classic Colorado views.", active: true, image: 'https://www.voc.org/sites/default/files/styles/760x420/public/op_images/ShelfLake_8.jpg?itok=_ZH6V4li')
      @address_1 = Address.create!(owner: @project_1, line_1: "Shelf Lake Trailhead", line_2: "Co Rd 1038", city: "Grant", state: "CO", zip: "80448")
 
      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
@@ -25,7 +25,7 @@ describe 'As an admin' do
      expect(current_path).to eq(edit_admin_project_path(@project_1))
      expect(page.find_field('Title').value).to eq(@project_1.title)
      expect(page.find_field('Description').value).to eq(@project_1.description)
-     expect(page.find_field('Date').value).to eq(future_date)
+     expect(page.find_field('Date').value).to eq(@future_date)
 
      expect(page.find_field('Line 1').value).to eq(@project_1.location.line_1)
      expect(page.find_field('Line 2').value).to eq(@project_1.location.line_2)
@@ -67,7 +67,7 @@ describe 'As an admin' do
     expect(current_path).to eq(admin_project_path(@project_1))
     expect(page).to have_content("Location line 1 can't be blank")
     expect(page.find_field('Title').value).to eq(@project_1.title)
-    expect(page.find_field('Date').value).to eq(future_date)
+    expect(page.find_field('Date').value).to eq(@future_date)
   end
 
 end
